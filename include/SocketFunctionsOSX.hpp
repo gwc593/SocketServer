fc4704094ad41a8b03f632d961f56def0d80d58a
gwc593 <gwc593@gmail.com>
2018-08-06 17:46:03 +0100

    Added client object functionality.

--- include/SocketFunctionsOSX.hpp
+++ include/SocketFunctionsOSX.hpp
@@ -11,25 +11,22 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include<arpa/inet.h>
 
-void welcome_message();
 
 
 // Author: Guy Collins
 // Date: 06/08/2018
-// Description: function to set up a handle to a new socket for listening.
-int creatSocketServer(int PORT);
-
-
+// Description: Object orientated socket server wrapper.
 class ServerSocket
 {
-    private:
-    int server_fd, socketHandle, PORT;
+private:
+    int server_fd, socketHandle, _PORT;
 
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    public:
+public:
 
     void setPort(int port);
 
@@ -39,6 +36,36 @@ class ServerSocket
 
     void activeSend(char* buffer, int buffLen);
 };
+
+// Author: Guy Collins
+// Date: 06/08/2018
+// Description: Object orientated socket server wrapper.
+class ClientSocket
+{
+private:
+    int _PORT;
+    char _ip[12];
+
+    int socketHandle = 0;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+
+public:
+
+    void setPort(int port);
+
+    void setIP(char* ip);
+
+    void setup (char* ip, int port);
+
+    int init();
+
+    void activeSend(char* buffer, int buffLen);
+
+    void passiveRead(char* buffer, int buffLen);
+};
+
 
 
 #endif //SOCKET_LISTEN_SOCKETFUNCTIONSOSX_HPP
--- src/SocketFunctionsOSX.cpp
+++ src/SocketFunctionsOSX.cpp
@@ -5,41 +5,18 @@
 #include <SocketFunctionsOSX.hpp>
 
 
-void welcome_message()
-{
-    printf("this is working now...\n");
-}
 
 // Author: Guy Collins
 // Date: 06/08/2018
-// Description: function to set up a handle to a new socket for listening.
-int creatSocketServer(int PORT)
-{
-    int server_fd, new_socket, valread;
-
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    return server_fd;
-}
+//Class: ServerSocket
+// Description: Object orientated socket wrapper.
 
 void ServerSocket::setPort(int port)
 {
-    PORT = port;
+    _PORT = port;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons( _PORT );
 }
 
 int ServerSocket::init()
@@ -83,4 +60,58 @@ void ServerSocket::passiveRead(char *buffer, int buffLen)
 void ServerSocket::activeSend(char *buffer, int buffLen)
 {
     send(socketHandle,buffer,buffLen,0);
+}
+
+
+void ClientSocket::setPort(int port)
+{
+    _PORT = port;
+}
+
+void ClientSocket::setIP(char* ip)
+{
+    strcpy(_ip,ip);
+}
+
+void ClientSocket::setup(char* ip, int port)
+{
+    _PORT = port;
+    strcpy(_ip,ip);
+}
+
+int ClientSocket::init()
+{
+    if ((socketHandle = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(_PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(socketHandle, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+}
+
+void ClientSocket::activeSend(char *buffer, int buffLen)
+{
+    send(socketHandle,buffer,buffLen,0);
+}
+
+void ClientSocket::passiveRead(char *buffer, int buffLen)
+{
+    read(socketHandle,buffer,buffLen);
 }
